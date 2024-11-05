c1b82842c7d806d5ee3c62237bb580ee0afba78d
gabrielcarballosa <gabriel.carballosa@edu.uah.es>
2024-11-05 17:51:11 +0100

    siu

--- src/ArbolHuffman.scala
+++ src/ArbolHuffman.scala
@@ -11,6 +11,8 @@ trait ArbolHuffman {
 
     caracteresAux(this, "").toList
 
+
+
   type Bit = 0 | 1
 
   def decodificar(bits: List[Bit]): String =
@@ -24,6 +26,7 @@ trait ArbolHuffman {
 
     decodificarAux(this, "", bits)
 
+  //Funcion de ayuda para codificar
   def estaEnArbol(char: Char): Boolean = this match
     case RamaHuffman(nodoIzq, nodoDch) => caracteres.contains(char)
     case HojaHuffman(c, p) => c == char
@@ -32,15 +35,17 @@ trait ArbolHuffman {
   def codificar(cadena: String): List[Bit] =
     def auxCodificar(arbolito: ArbolHuffman, listChar: List[Char], res: List[Bit]): List[Bit] = arbolito match
       case RamaHuffman(nodoIzq, nodoDch) if listChar != Nil =>
-        if nodoDch.estaEnArbol(listChar.head) then auxCodificar(nodoDch, listChar, res :+ 1)
-        else if nodoIzq.estaEnArbol(listChar.head) then auxCodificar(nodoIzq, listChar, res :+ 0)
+        if nodoDch.estaEnArbol(listChar.head) then auxCodificar(nodoDch, listChar, 1::res)
+        else if nodoIzq.estaEnArbol(listChar.head) then auxCodificar(nodoIzq, listChar, 0::res)
         else throw new Exception("No existe ese caracter en el arbol")
 
       case HojaHuffman(c, p) if listChar != Nil => auxCodificar(this, listChar.tail, res)
 
-      case _ => res
+      case _ => res.reverse
 
     auxCodificar(this, cadena.toList, List())
+
+
 }
 
 
@@ -108,14 +113,51 @@ def crearArbolHuffman(cadena: String): ArbolHuffman =
   
   val ordenFrec = DistribFrecAListaHojas(frec)
   repetirHasta(combinar,esListaSingleton)(ordenFrec)
-  
+
+
+
+object ArbolHuffman {
+  def apply(cadena: String): ArbolHuffman = crearArbolHuffman(cadena)
+
+}
+
+
+type Bit = 0|1
+
+type TablaCodigos = List[(Char, List[Bit])]
+
+def deArbolATabla(arbol: ArbolHuffman): TablaCodigos =
+  val caracteres = arbol.caracteres
+  def aux(list: TablaCodigos,chars : List[Char]):TablaCodigos = chars match
+    case Nil => list
+    case h::t => aux((h,arbol.codificar(h.toString))::list,t)
+
+  aux(Nil,caracteres)
+
+def codificarT(arbol: TablaCodigos)(cadena: String): List[Bit]=
+  def aux(cad : List[Char],res : List[Bit],arbolaux: TablaCodigos):List[Bit]=arbolaux match
+    case Nil => if cad == Nil then res.reverse else throw new Exception("No esta esa letra")
+    case h::t if cad != Nil => if cad.head == h._1 then aux(cad.tail,h._2.reverse ::: res,arbol) else aux(cad,res,t)
+    case _ => res.reverse
+  aux(cadena.toList,Nil,arbol)
+
+def decodificarT(tabla: TablaCodigos)(bits: List[Bit]): String=
+  def buscar (bit: Bit,tablita: TablaCodigos,res: TablaCodigos):TablaCodigos = tablita match
+    case Nil => res
+    case h::t => if h._2.head == bit then buscar(bit,t,(h._1,h._2.tail) :: res) else buscar(bit, t, res)
+
+  def aux (tablaAux: TablaCodigos, bitsAux: List[Bit], cadena: String):String= bitsAux match
+    case h::t if tablaAux.size == 1 => aux(tabla,bitsAux,cadena+tablaAux.head._1)
+    case h::t => aux(buscar(h,tablaAux,Nil),t,cadena)
+    case Nil => cadena+tablaAux.head._1
+  aux(tabla,bits,"")
 
 
 
 
 object miPrograma extends ArbolHuffman, App {
 
-
+  //Prueba primera parte para ver si esta bien la estructura del arbol
   val n1 = HojaHuffman(' ', 2)
   val n2 = HojaHuffman('e', 2)
   val r1 = RamaHuffman(n2, n1)
@@ -123,21 +165,37 @@ object miPrograma extends ArbolHuffman, App {
   val r2 = RamaHuffman(n3, r1)
   val n4 = HojaHuffman('s', 4)
   val r3 = RamaHuffman(n4, r2)
+  println(r3)
+
+  //Prueba de sus metodos peso y caracteres
   val pes= r3.peso
   val cad = r3.caracteres
   println(pes)
   println(cad)
 
-
+  //Prueba de los metodos codificar y decodificar
   val mensaje = r3.decodificar(List(0,1,0,0,1,1,1,1,1,0,0,1,1,0,1,1,1,1,0,0))
   println(mensaje)
-
   val mensajeCod = r3.codificar(mensaje)
   println(mensajeCod)
 
+  //Prueba de crear un arbol y comprobacion de que codifica y decodifica bien
   val prueba = "hola bbe"
   val arbolPrueba = crearArbolHuffman(prueba)
   println(arbolPrueba.codificar("hhob ee"))
   println(arbolPrueba.decodificar(List(0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1)))
+
+  //Prueba de la ultima parte de la practica que incluye instanciar
+  //un arbol, pasarlo a tabla, codificarlo y decodificarlo mediante la tabla
+  val miArbol = ArbolHuffman("texto para construir el arbol huffman")
+  println(deArbolATabla(miArbol))
+  val codMiArbol = codificarT(deArbolATabla(miArbol))
+  println(codMiArbol("texto arbol manhuff"))
+  val decoMiArbol = decodificarT(deArbolATabla(miArbol))
+  println(decoMiArbol(List(1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0
+    , 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1,
+    0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0)))
+
+
 }
 
